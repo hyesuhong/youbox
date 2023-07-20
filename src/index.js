aa2d9486829955366cc2adffc8eabd5b7b06a5d1
Hyesu <honghs95@gmail.com>
2023-07-20 13:14:42 +0900

    change file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 8080;
+
+/* const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+};
+
+// const privateMiddleware = (req, res, next) => {
+// 	const { url } = req;
+// 	if (url === '/protected') {
+// 		return res.send('<h1>Not allowed</h1>');
+// 	}
+// 	next();
+// };
+
+app.use(logger);
+// app.use(privateMiddleware); */
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
 	// return res.end();
