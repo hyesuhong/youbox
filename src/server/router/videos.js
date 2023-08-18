4dea8e42e0f1bf2964b6f9836960ac65e65f3e0c
Hyesu <honghs95@gmail.com>
2023-08-18 13:42:18 +0900

    feat: add protector middleware to video router

--- src/server/router/videos.js
+++ src/server/router/videos.js
@@ -7,10 +7,9 @@ import {
 	postUpload,
 	deleteVideo,
 } from '../controller/videos';
+import { protectorMiddleware } from '../middleware/locals';
 
 const videosRouter = express.Router();
-
-// const idParams = ':id(\\d+)';
 
 /* mongoose's ObjectId = 24 hexadecimal character string  */
 const idParams = ':id([0-9a-f]{24})';
@@ -21,17 +20,20 @@ videosRouter.get(`/${idParams}`, getWatch);
 /* GET & POST: edit one video */
 videosRouter
 	.route(`/${idParams}/edit`) //
+	.all(protectorMiddleware)
 	.get(getEdit) //
 	.post(postEdit);
 
 /* GET: delete a video */
 videosRouter
 	.route(`/${idParams}/delete`) //
+	.all(protectorMiddleware)
 	.get(deleteVideo);
 
 /* GET & POST: upload a video */
 videosRouter
 	.route('/upload') //
+	.all(protectorMiddleware)
 	.get(getUpload) //
 	.post(postUpload);
 
